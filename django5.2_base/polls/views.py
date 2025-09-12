89100b67c7e49283e3e9ae69640d13a8fbdc4946
CHXN <2934387769@qq.com>
2025-09-12 21:34:31 +0800

    part5

--- django5.2_base/polls/views.py
+++ django5.2_base/polls/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 from .models import Choice, Question
 from django.template import loader
 from django.db.models import F
+from django.utils import timezone
 # Create your views here.
 
 
@@ -27,15 +28,19 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
     
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
     
 class ResultView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
