16c3c92b5557cfdff142c30037fc546d4f97a166
CHXN <2934387769@qq.com>
2025-09-13 12:43:01 +0800

    part7

--- django5.2_base/polls/admin.py
+++ django5.2_base/polls/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from polls import models
 # Register your models here.
-admin.site.register(models.Question)
+
+class ChoiceAdmin(admin.TabularInline):
+    model = models.Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date']})
+    ]
+
+    list_display = ['question_text','pub_date','was_published_recently']
+    list_filter = ["pub_date"]
+    inlines = [ChoiceAdmin]
+    search_fields = ["question_text"]
+
+admin.site.register(models.Question,QuestionAdmin)
