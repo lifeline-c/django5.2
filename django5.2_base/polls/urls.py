a303676251c0de69d304e4cece5e73afc90eea5d
CHXN <2934387769@qq.com>
2025-09-13 13:06:54 +0800

    part8

--- django5.2_base/polls/urls.py
+++ django5.2_base/polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from debug_toolbar.toolbar import debug_toolbar_urls
 app_name = "polls"
 urlpatterns=[
     path("",views.IndexView.as_view(),name="index"),
@@ -7,4 +8,4 @@ urlpatterns=[
     path("specifics/<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
-] 
+] + debug_toolbar_urls()
